c967ec9f37febfe36fb47ee841f5421d1a7dc4b3
Пользователь <bekzodyigitaliyev18@gmail.com>
2022-06-30 00:09:34 +0500

    uy iwi sams cars

---
+++ src/App.js
@@ -0,0 +1,47 @@
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className='birinchi'>
+        <div className='navbar'>
+          <div className='sams-auta'>
+            <span>Sams Auto</span>
+          </div>
+          <div className='links'>
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>Rent</a></li>
+            <li><a href='#'>Land</a></li>
+            <li><a href='#'>Agent</a></li>
+            <li><a href='#'>Contact Us</a></li>
+          </div>
+          <div className='buttun'>
+            <button>Buy CARS</button>
+          </div>
+        </div>
+        <div>
+          <div className='text-container'>
+            <h1>Revolutionary
+              Transportation at yourfingertips</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur 
+              adipiscing elit. Etiam phasellus tellus
+               adipiscing lectus dignissim faucibus at</p>
+               <div>
+                <button>Learn More  </button>
+                <button>Order Now</button>
+               </div>
+                <h4>Download our app now and enjoy our
+                   Amazing sign up bonuses</h4>
+                  <img src='Subtract(3).svg'></img>             
+          </div>
+          <div className='./rasmlar./logo go.svg'>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
